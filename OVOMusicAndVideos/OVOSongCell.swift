ccd01f1a6884064bdaabb19e82d3116b1104e7ac
Mikael <miketekle14@gmail.com>
2016-09-12 16:38:57 -0700

    created tableviewcell custom class

---
+++ OVOMusicAndVideos/OVOSongCell.swift
@@ -0,0 +1,26 @@
+//
+//  OVOSongCell.swift
+//  OVOMusicAndVideos
+//
+//  Created by Mikael Teklehaimanot on 9/12/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+
+class OVOSongCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
