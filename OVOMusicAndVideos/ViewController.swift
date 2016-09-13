dc821b46840cc0f4e68eb41f6bd77ab0f51643d3
Mikael <miketekle14@gmail.com>
2016-09-12 17:36:48 -0700

    configured table view to use custom cell when dequeuing reusable cells, hardcoded data into tableview datasource for testing purposes

--- OVOMusicAndVideos/ViewController.swift
+++ OVOMusicAndVideos/ViewController.swift
@@ -11,10 +11,20 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var tableData = [OVOMusic]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //=============================================================================
+        //test if things are working by hard coding objects into array
+        let ovoSong1 = OVOMusic(videoTitle: "Hotline Bling", imageURL: "", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/uxpDa-c-4Mc\" frameborder=\"0\" allowfullscreen></iframe>")
+        tableData.append(ovoSong1)
+        //=================^^^HARDCODED DATA^^^========================================
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,11 +33,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return tableData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "OVOSongCell", for: indexPath) as? OVOSongCell {
+            
+            cell.updateUI(ovoSong: tableData[indexPath.row])
+            print(cell)
+            return cell
+            
+        } else {
+            return UITableViewCell()
+        }
     }
     
 }
