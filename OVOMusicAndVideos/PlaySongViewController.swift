b6ac3d70c84b011779862ea29ad6d52c2a43b483
Mikael <miketekle14@gmail.com>
2016-09-13 12:38:06 -0700

    minor refactoring

--- OVOMusicAndVideos/PlaySongViewController.swift
+++ OVOMusicAndVideos/PlaySongViewController.swift
@@ -34,7 +34,7 @@ class PlaySongViewController: UIViewController, UIWebViewDelegate {
     
     
     func webViewDidFinishLoad(_ webView: UIWebView) {
-        //TODO: Make resposive YouTube video for webview
+        //custom css and js to make video url content responsive to webview
         let css = ".container { position: relative; width: 100%; height: 0; padding-bottom: 56.25%; } .video { position: absolute; top: 0; left: 0; width: 100%; height: 100%;}"
         
         let js = "var styleNode = document.createElement('style'); styleNode.type = \"text/css\"; var styleText = document.createTextNode('\(css)'); styleNode.appendChild(styleText); document.getElementsByTagName('head')[0].appendChild(styleNode);"
